e7d27e00bd23d98c2717c478cab2ba23672be752
Alexey Gorokhov <car1es@bk.ru>
2022-10-26 18:59:15 +0300

    initial commit

---
+++ Main.java
@@ -0,0 +1,43 @@
+package streams.task2;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> names = Arrays.asList("Jack", "Connor", "Harry", "George", "Samuel", "John");
+        List<String> families = Arrays.asList("Evans", "Young", "Harris", "Wilson", "Davies", "Adamson", "Brown");
+        Collection<Person> persons = new ArrayList<>();
+        for (int i = 0; i < 10_000_000; i++) {
+            persons.add(new Person(
+                    names.get(new Random().nextInt(names.size())),
+                    families.get(new Random().nextInt(families.size())),
+                    new Random().nextInt(100),
+                    Sex.values()[new Random().nextInt(Sex.values().length)],
+                    Education.values()[new Random().nextInt(Education.values().length)])
+            );
+        }
+
+        long count = persons.stream()
+                .filter(person -> person.getAge() < 18)
+                .count();
+        System.out.println(count);
+
+        List<String> conscript = persons.stream()
+                .filter(person ->
+                        person.getSex().equals(Sex.MAN) && person.getAge() >= 18 && person.getAge() < 27)
+                .map(person -> person.getFamily())
+                .collect(Collectors.toList());
+        System.out.println(conscript);
+
+        List<Person> jobPerson = persons.stream()
+                .filter(person -> person.getAge() > 18)
+                .filter(person -> person.getEducation().equals(Education.HIGHER))
+                .filter(person ->
+                        person.getSex().equals(Sex.WOMAN) && person.getAge() <= 60 ||
+                                person.getSex().equals(Sex.MAN) && person.getAge() <= 65)
+                .sorted(Comparator.comparing(person -> person.getFamily()))
+                .collect(Collectors.toList());
+
+    }
+}
---
+++ Sex.java
@@ -0,0 +1,6 @@
+package streams.task2;
+
+public enum Sex {
+    MAN,
+    WOMAN
+}
